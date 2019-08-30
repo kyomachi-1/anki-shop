b1e0716759e013ba3fb6361f0e143b6e8362739b
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-08-31 08:08:42 +0900

    add jumbotron for web/index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -3,5 +3,11 @@
 @section('content')
 
 <h1>Web#Index</h1>
+<div class="jumbotron jumbotron-fruid">
+    <div class="container">
+        <h1 class="display-4">AnkiShop</h1>
+        <p>Developed by PHP/Laravel/Vue.js/PAY.JP</p>
+    </div>
+</div>
 
 @endsection 
