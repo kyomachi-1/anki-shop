f9cb2e07192e55207355926a9f9532a4ee9bb770
kyomachi-1 <kyomachi.so.dev1@gmail.com>
2019-09-17 18:54:44 +0900

    merge-test

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -5,7 +5,7 @@
 <h1>Web#Index</h1>
 <div class="jumbotron jumbotron-fluid">
     <div class="container">
-        <h1 class="display-4">AnkiShop</h1>
+        <h1 class="display-4">SampleShop</h1>
         <p>Developed by PHP/Laravel/Vue.js/PAY.JP</p>
     </div>
 </div>
